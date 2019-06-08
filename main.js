c78a54900c74351b785e10526d67e78e023f1a94
Alvin <alvin.qin.ca@gmail.com>
2019-06-07 17:57:14 -0700

    complete functionalities

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
+document.getElementById('btn').addEventListener('click', function () {
+  var todo = document.getElementById("todo").value
+  document.getElementById("todo").value = ""
+  if (todo != "") {
+    var todoList = document.getElementById('todo-list')
 
-
-
-
-
-
-document.getElementById('btn').addEventListener('click', function() {
-    var todo = document.getElementById("todo").value
-    if (todo != "") {
-      var container = document.getElementById('todo-container')
-    
     var newTodo = document.createElement('li')
-    newTodo.innerText = todo
-    
+    newTodo.innerHTML = '<label>' + todo + '</lable>'
+
+    var checkBox = document.createElement('input')
+    checkBox.type = "checkbox"
+
     var RemoveBtn = document.createElement('button')
     RemoveBtn.innerText = 'Remove'
-    RemoveBtn.addEventListener ('click', function () {
-      
-      container.removeChild(newTodo);
+    RemoveBtn.addEventListener('click', function () {
+
+      todoList.removeChild(newTodo);
     })
-    container.appendChild(newTodo)
+    todoList.prepend(newTodo)
+    newTodo.prepend(checkBox)
+    
+    checkBox.addEventListener('change', function () {
+      if (this.checked) {
+        newTodo.childNodes[1].style.textDecoration = "line-through";
+      } else {
+        newTodo.childNodes[1].style.textDecoration = "";
+      }
+    });
     newTodo.appendChild(RemoveBtn)
-    }
+    console.log(newTodo.childNodes[1])
+  }
+  document.getElementById('clear-btn').addEventListener('click', function () {
+    var todoList = document.getElementById('todo-list')
+    todoList.innerHTML = ''
   })
+})
+
+
+
